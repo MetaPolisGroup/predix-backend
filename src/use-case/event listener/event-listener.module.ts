c34eb538ec8587f6ee6b3d7952d8f18d829ee813
nbi67 <nbi67@NYNEO>
2023-07-25 14:12:03 +0700

    chore

--- src/use-case/event listener/event-listener.module.ts
+++ src/use-case/event listener/event-listener.module.ts
@@ -2,11 +2,12 @@ import { Module } from '@nestjs/common';
 import { EventBetListener } from './event-bet.service';
 import { EventRoundListener } from './event-round.service';
 import { EventClaimListener } from './event.claim.service';
+import { PredictionModule } from '../prediction/prediction.module';
 
 @Module({
   providers: [EventBetListener, EventRoundListener, EventClaimListener],
   controllers: [],
-  imports: [],
+  imports: [PredictionModule],
   exports: [],
 })
 export class EventListenerModule {}
