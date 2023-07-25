b254d48ddb735dacd437f2271a4148886ae26634
dotrong124 <dotrong124@gmail.com>
2023-07-25 10:12:49 +0700

    update: create user

--- src/controller/user.controller.ts
+++ src/controller/user.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post, Req } from '@nestjs/common';
+import { Body, Controller, Param, Post, Req } from '@nestjs/common';
 import { Request } from 'express';
 import { CreateUserDto } from 'src/core/dtos/user/user.dto';
 import { User } from 'src/core/entity/user.enity';
@@ -8,7 +8,12 @@ import { UserService } from 'src/use-case/user/user.service';
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
-  @Post('/create')
+  @Post('/create/: id')
+  async createByRef(@Body() dto: CreateUserDto, @Param('id') id: string, @Req() req: Request) {
+    return this.userService.create(dto, req, id);
+  }
+
+  @Post('/create/')
   async create(@Body() dto: CreateUserDto, @Req() req: Request) {
     return this.userService.create(dto, req);
   }
