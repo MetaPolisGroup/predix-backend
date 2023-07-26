09265b535d61b5efcf8bd7ab75bb49e4336278ec
dotrong124 <dotrong124@gmail.com>
2023-07-26 09:21:15 +0700

    update: entity leaderboard

---
+++ src/core/entity/leaderboard.entity.ts
@@ -0,0 +1,13 @@
+import { LeaderBoardUser } from './user.enity';
+
+export class Leaderboard {
+  id?: string;
+
+  user_list: { leaderboard: LeaderBoardUser; user_id: string }[];
+
+  type: LeaderBoardType;
+
+  updated_at: number;
+}
+
+export type LeaderBoardType = 'Total BNB' | 'Rounds Played' | 'Net Winnings' | 'Win Rate';
