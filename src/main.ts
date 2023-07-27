074c587e66cc4ad4084b969f55df6c2fbd92b84d
annt <an.nt.techdev@gmail.com>
2023-07-27 14:07:25 +0700

    chore: fix cors

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,28 @@
-import { NestFactory } from '@nestjs/core';
+import { HttpAdapterHost, NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { AllExceptionsFilter } from './core/exception handler/all-exceptions-filter';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
+  const httpAdapter = app.get(HttpAdapterHost);
+
+  // Global Exception Filters
+  app.useGlobalFilters(new AllExceptionsFilter(httpAdapter));
+
+  // Global Validation Pipe
+  app.useGlobalPipes(
+    new ValidationPipe({
+      enableDebugMessages: true,
+      transform: true,
+      whitelist: true,
+      transformOptions: {
+        enableImplicitConversion: true, // allow conversion underneath
+      },
+    }),
+  );
+
   await app.listen(3000);
 }
+
 bootstrap();
