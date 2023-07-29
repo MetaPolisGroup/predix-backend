205e47cdfd117e0e22677f6e3405745be9c6e3c9
nbi67 <nbi67@NYNEO>
2023-07-29 14:03:59 +0700

    update: address contract

--- src/configuration/address/prediction-address.ts
+++ src/configuration/address/prediction-address.ts
@@ -1,6 +1,6 @@
 import { ChainType } from '../chain';
 
 export const predictionAddr = {
-  [ChainType.BSCTESTNET]: '0x7560AEb7ff8647d58B4f053848DE523cd60330A4' as `0x${string}`,
+  [ChainType.BSCTESTNET]: '0x94F963651E85836E3784fB8eA28FBCb3cf1B7Df0' as `0x${string}`,
   [ChainType.BSC]: '',
 };
--- src/configuration/address/token-address.ts
+++ src/configuration/address/token-address.ts
@@ -1,6 +1,6 @@
 import { ChainType } from '../chain';
 
 export const tokenAddr = {
-  [ChainType.BSCTESTNET]: '0xa3dca1Dc73A9bEaaf72e009162f054BBffa18E39' as `0x${string}`,
+  [ChainType.BSCTESTNET]: '0xd82A54b913c44777f37d2f0cE1bC2eC6F427574b' as `0x${string}`,
   [ChainType.BSC]: '',
 };
