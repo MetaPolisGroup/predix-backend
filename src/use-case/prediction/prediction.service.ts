7d71f11ea9682b3a2b1a68a88fcb1a275da1e8c6
nbi67 <nbi67@NYNEO>
2023-07-29 10:13:09 +0700

    fix

--- src/use-case/prediction/prediction.service.ts
+++ src/use-case/prediction/prediction.service.ts
@@ -91,7 +91,7 @@ export class PredictionService implements OnApplicationBootstrap {
       this.logger.error(`Preferences not found when set cronjob`);
     }
 
-    if (this.cronJobs[availableRound.epoch]) {
+    if (this.cronJobs[availableRound?.epoch]) {
       this.logger.log(`Cronjob for round ${availableRound.epoch} have already set !`);
     }
   }
