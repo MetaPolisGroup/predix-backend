ff9ccb545b5c74ae7a4aee3028d7a52060411863
nbi67 <nbi67@NYNEO>
2024-03-10 10:48:30 +0700

    chore: change contract addresses

--- src/configuration/address/token-address.ts
+++ src/configuration/address/token-address.ts
@@ -3,6 +3,6 @@ import { ChainType } from '../chain';
 export const tokenAddr = {
   [ChainType.BSCTESTNET]: '0xb90925393aBe64f9db1A0200a09C9162F334d5a8' as `0x${string}`,
   [ChainType.BASETESTNET]: '0x17545B2C0C56b8f284355DF2C8062e167612AEE1' as `0x${string}`,
-  [ChainType.BASESEPOLIA]: '0xb728609D181ad7D8BE8dc049130c081a031B0f95' as `0x${string}`,
+  [ChainType.BASESEPOLIA]: '0x042a75F06824B35bfabd93355Fa250b7106Ffd28' as `0x${string}`,
   [ChainType.BSC]: '1',
 };
