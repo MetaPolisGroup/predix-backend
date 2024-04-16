42559e7f2f1a8e5e231de978690967f750307b13
nbi67 <nbi67@NYNEO>
2024-04-16 11:16:18 +0700

    chore: re-config & remove junk files

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,38 +1,54 @@
 module.exports = {
-  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended-type-checked', 'plugin:prettier/recommended', 'prettier'],
-
-  plugins: ['@typescript-eslint'],
-
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    project: true,
-    tsconfigRootDir: __dirname,
-  },
-  root: true,
-  env: {
-    node: true,
-    jest: true,
-  },
-  ignorePatterns: ['.eslintrc.js'],
-  rules: {
-    '@typescript-eslint/ban-types': 'error',
-    '@typescript-eslint/explicit-function-return-type': 'off',
-    '@typescript-eslint/no-misused-promises': [
-      'off',
-      {
-        checksConditionals: false,
-      },
+    extends: [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended-type-checked',
+        'plugin:prettier/recommended',
     ],
 
-    'no-duplicate-imports': 'error',
-    '@typescript-eslint/no-unsafe-call': 'off',
-    '@typescript-eslint/no-unsafe-return': 'off',
-    '@typescript-eslint/no-unsafe-assignment': 'off',
-    '@typescript-eslint/no-unsafe-member-access': 'off',
-    '@typescript-eslint/explicit-module-boundary-types': 'off',
-    '@typescript-eslint/no-explicit-any': 'off',
-    '@typescript-eslint/no-empty-function': 'off',
-    '@typescript-eslint/no-unused-vars': 'off',
-    'lines-between-class-members': ['error', 'always'],
-  },
+    plugins: ['@typescript-eslint'],
+
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        project: true,
+        tsconfigRootDir: __dirname,
+    },
+    //     root: true,
+    env: {
+        node: true,
+        jest: true,
+    },
+    ignorePatterns: ['.eslintrc.js'],
+
+    rules: {
+        '@typescript-eslint/ban-types': 'error',
+        '@typescript-eslint/explicit-function-return-type': 'off',
+        '@typescript-eslint/no-misused-promises': [
+            'off',
+            {
+                checksConditionals: false,
+            },
+        ],
+        'prettier/prettier': [
+            'error',
+            {},
+            {
+                usePrettierrc: true,
+            },
+        ],
+        'no-duplicate-imports': 'error',
+        'lines-between-class-members': ['error', 'always'],
+        'require-await': 'off',
+        '@typescript-eslint/no-unsafe-call': 'warn',
+        '@typescript-eslint/no-unsafe-return': 'warn',
+        '@typescript-eslint/no-unsafe-member-access': 'warn',
+        '@typescript-eslint/no-explicit-any': 'warn',
+        '@typescript-eslint/no-unused-vars': 'warn',
+        '@typescript-eslint/no-floating-promises': 'off',
+        '@typescript-eslint/no-empty-function': 'off',
+        '@typescript-eslint/no-unsafe-assignment': 'off',
+        '@typescript-eslint/explicit-module-boundary-types': 'off',
+        'no-return-await': 'off',
+        '@typescript-eslint/return-await': 'error',
+        '@typescript-eslint/ban-types': 'error',
+    },
 };
